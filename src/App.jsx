649951211cd5bd88aa1ba0871f610ca85499dadf
kartik-613 <kartikupadhyay613@gmail.com>
2025-10-06 13:58:44 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Login from "./pages/Login";
-import Form from "./pages/Form";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
